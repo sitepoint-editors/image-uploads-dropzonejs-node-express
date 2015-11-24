56f93aa13dfd43c7e94fe0f2b038bcc56657756c
Marcell <kissmarcell88@gmail.com>
2015-11-24 08:59:31 +0100

    removing handlebar

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ var express   =   require( 'express' );
 var multer    =   require( 'multer' );
 var upload    =   multer( { dest: 'uploads/' } );
 var sizeOf    =   require( 'image-size' );
-var exphbs    =   require( 'express-handlebars' );
 require( 'string.prototype.startswith' );
 
 var app = express();
 
 app.use( express.static( __dirname + '/bower_components' ) );
 
-app.engine( '.hbs', exphbs( { extname: '.hbs' } ) );
-app.set('view engine', '.hbs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.get( '/', function( req, res, next ){
   return res.render( 'index' );
