4434f9420655fb1a58b9d04f414b532f1f20a2a9
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2020-01-27 12:44:14 +0100

    Updates for republishing

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
-#Image Uploading with Dropzone.js, Node.js and Express
+# Image Uploading with Dropzone.js, Node.js and Express
 
 This simple example of an image upload facility is designed to accompany a SitePoint article on [DropzoneJS](http://www.dropzonejs.com). The server-side component is written using Node.js and Express.
 
 Article URL: http://www.sitepoint.com/file-upload-form-express-dropzone-js/
 
-Full details will be in the article, but here's a quickstart:
+## Requirements
+
+* [Node.js](http://nodejs.org/)
+
+## Installation Steps (if applicable)
 
 1. Clone the repository
 2. Ensure that `uploads/` is writeable
-3. Optionally run `vagrant up`, then `vagrant ssh`, then `cd /vagrant`
-4. Run `npm install`
-5. Run `bower install`
-6. Run the app with `node index.js`
-7. The app will run on port 8080
+3. Run `npm install`
+4. Run the app with `node index.js`
+5. The app will run on port 8080. Try uploading an image.
+
+## License
+
+SitePoint's code archives and code examples are licensed under the MIT license.
+
+Copyright © 2020 SitePoint
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-var express   =   require( 'express' );
-var multer    =   require( 'multer' );
-var upload    =   multer( { dest: 'uploads/' } );
-var sizeOf    =   require( 'image-size' );
-var exphbs    =   require( 'express-handlebars' );
-require( 'string.prototype.startswith' );
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest:'uploads/'});
+const sizeOf = require('image-size');
+const exphbs = require('express-handlebars');
 
-var app = express();
+const app = express();
 
-app.use( express.static( __dirname + '/bower_components' ) );
+app.use(express.static(__dirname +'/public'));
 
-app.engine( '.hbs', exphbs( { extname: '.hbs' } ) );
-app.set('view engine', '.hbs');
+app.engine('.hbs', exphbs({ extname:'.hbs'}));
+app.set('view engine','.hbs');
 
-app.get( '/', function( req, res, next ){
-  return res.render( 'index' );
+app.get('/', (req, res) => {
+  return res.render('index', {layout: false});
 });
 
-app.post( '/upload', upload.single( 'file' ), function( req, res, next ) {
-
-  if ( !req.file.mimetype.startsWith( 'image/' ) ) {
-    return res.status( 422 ).json( {
-      error : 'The uploaded file must be an image'
-    } );
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file.mimetype.startsWith('image/')) {
+    return res.status(422).json({
+      error :'The uploaded file must be an image'
+    });
   }
 
-  var dimensions = sizeOf( req.file.path );
+  const dimensions = sizeOf(req.file.path);
 
-  if ( ( dimensions.width < 640 ) || ( dimensions.height < 480 ) ) {
-    return res.status( 422 ).json( {
-      error : 'The image must be at least 640 x 480px'
-    } );
+  if ((dimensions.width < 640) || (dimensions.height < 480)) {
+    return res.status(422).json({
+      error :'The image must be at least 640 x 480px'
+    });
   }
 
-  return res.status( 200 ).send( req.file );
+  return res.status(200).send(req.file);
 });
 
-app.listen( 8080, function() {
-  console.log( 'Express server listening on port 8080' );
+app.listen(8080, () => {
+  console.log('Express server listening on port 8080');
 });
